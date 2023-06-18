3cee4571013b63d8e2c0331c4897c673d12e6b0f
chrisjrocen <chrisocen4@gmail.com>
2023-06-18 20:12:42 +0300

    added question to fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,1 +1,6 @@
-
+/**
+ * 1. Write a function named fizzBuzz that takes in two(2) parameters which
+are expected to be strings. The function should return the string Fizz if
+the combined length of the parameters is divisible by 3, the string Buzz
+if it is divisible by 5, and the string FizzBuzz if it is divisible by both 5 and
+ */
