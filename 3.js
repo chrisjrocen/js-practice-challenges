c5dd0cf7f986b911d535ea25ca98a4f6e34b3cc6
Bryan <abalebrian@gmail.com>
2023-06-28 11:47:44 +0300

    solution for number 3

---
+++ 3.js
@@ -0,0 +1,9 @@
+function twoSum(nums,target ){
+    for(let i = 0; i <= nums.length; i++){
+        for(let j = 0; j <= nums.length; j++){
+            if(nums[i] + nums[j] == target){
+                return [i, j]
+            }
+        }
+    }
+};
